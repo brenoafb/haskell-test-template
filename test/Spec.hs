c66be8cc4de0cd09e6da4d04a00f00f507167ce8
breno.afb <breno.afb@gmail.com>
2022-01-13 18:08:27 -0300

    Add new tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,18 @@
-import Test.HUnit
+import Test.Hspec
 
 import Lib
 
+  -- We can run individual tests
+  -- Example
+  -- stack test --test-arguments='--match "myStr matches"'
 main :: IO ()
-main = runTestTT tests >> pure ()
-
-test1 = TestCase (assertEqual "myStr matches " "Hello world" myStr)
-tests = TestList [TestLabel "test1" test1]
+main = hspec $ do
+  describe "myStr" $ do
+    it "myStr matches \"Hello world\"" $ do
+      myStr `shouldBe` "Hello world"
+  describe "test 1" $ do
+    it "always passes" $ do
+      1 `shouldBe` 1
+  describe "test 2" $ do
+    it "always fails" $ do
+      1 `shouldBe` 2
